d6f9d49945d47ab39b95281f9aaa190183592f8d
THOMAS <thomaslunardo@gmail.com>
2024-01-31 23:25:08 +0100

    created a show method and it view for zoom on a specific spot. add some style to

--- src/Controller/SpotController.php
+++ src/Controller/SpotController.php
@@ -8,15 +8,31 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/Spot')]
 class SpotController extends AbstractController
 {
-    #[Route('/spots', name: 'app_spot')]
+    #[Route('/', name: 'app_spot')]
     public function index(SpotRepository $spotRepository): Response
     {
         $spots = $spotRepository->findAll();
 
         return $this->render('spot/index.html.twig', [
             'spots' => $spots,
+        ]);
+    }
+
+    #[Route('/show/{id<^[0-9]+$>}', name: 'app_spot_show')]
+    public function show(int $id, SpotRepository $spotRepository): Response
+    {
+        $spot = $spotRepository->findOneBy(['id' => $id]);
+
+        if (!$spot) {
+            throw $this->createNotFoundException(
+                'Auncun spot : ' . $id . ' trouvé dans la table spot.'
+            );
+        }
+        return $this->render('spot/show.html.twig', [
+        'spot' => $spot,
         ]);
     }
 }
