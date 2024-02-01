5765fc5af4765c69a562557620a383974545d198
THOMAS <thomaslunardo@gmail.com>
2024-02-02 00:28:05 +0100

    work on session controller for redirect on session index but it does work yet

--- src/Controller/SessionController.php
+++ src/Controller/SessionController.php
@@ -4,22 +4,55 @@ namespace App\Controller;
 
 use App\Entity\Session;
 use App\Form\SessionType;
+use App\Repository\SessionRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/Session')]
 class SessionController extends AbstractController
 {
-    #[Route('/session', name: 'app_session')]
-    public function index(): Response
+    #[Route('/new', name: 'app_session')]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
         $session = new Session();
 
         // Create the form, linked with $session
         $form = $this->createForm(SessionType::class, $session);
 
+        // Get data from HTTP request
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted()) {
+            $entityManager->persist($session);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Ta session a bien été enregistrée!');
+
+        // Redirect to session list
+            return $this->redirectToRoute('app_session_show', [], Response::HTTP_SEE_OTHER);
+        }
+
         return $this->render('session/index.html.twig', [
             'form' => $form,
+        ]);
+    }
+
+    #[Route('/show/{id<^[0-9]+$>}', name: 'app_session_show')]
+    public function show(int $id, SessionRepository $sessionRepository): Response
+    {
+        $session = $sessionRepository->findOneBy(['id' => $id]);
+
+        if (!$session) {
+            throw $this->createNotFoundException(
+                'Auncune session : ' . $id . ' trouvé dans la table session.'
+            );
+        }
+
+        return $this->render('session/show.html.twig', [
+        'session' => $session,
         ]);
     }
 }
